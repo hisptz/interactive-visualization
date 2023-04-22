d60c4aa690454051ce9b5fd37e51ca1e8051b4d1
gian <nnkogift@hotmail.com>
2023-04-22 12:49:44 +0300

    :bug: fix(welcome-page): Fixes
    - Fixed missing description of page

--- src/pages/WelcomePage/WelcomePage.js
+++ src/pages/WelcomePage/WelcomePage.js
@@ -7,9 +7,11 @@ export function WelcomePage() {
             height: "100%",
             alignItems: "center",
             justifyContent: "center",
-            display: "flex"
+            display: "flex",
+            flexDirection: "column"
         }}>
             <h1>Welcome Page</h1>
+            <p style={{margin: 0}}>This is the welcome page</p>
         </div>
     )
 }
