9c5acd2ef8bcd942ea16f931269be25eeff163ef
gian <nnkogift@hotmail.com>
2023-04-22 12:40:11 +0300

    :sparkles: feat(welcome-page): Implementation
    - Implemented minimal welcome page feature

---
+++ src/pages/WelcomePage/WelcomePage.js
@@ -0,0 +1,15 @@
+export function WelcomePage() {
+
+    return (
+        <div style={{
+            textAlign: 'center',
+            width: "100%",
+            height: "100%",
+            alignItems: "center",
+            justifyContent: "center",
+            display: "flex"
+        }}>
+            <h1>Welcome Page</h1>
+        </div>
+    )
+}
