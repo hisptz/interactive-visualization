5c61732d36cf0da959c3d0bcc503c0db7e3b7597
jdmwacha0076 <jdmwacha0076@gmail.com>
2023-05-02 01:20:45 +0300

    Refactor visualization.js to improve components alignment and prevent overlapping of containers on screen maximize/minimize.

--- src/pages/Visualization/Visualization.js
+++ src/pages/Visualization/Visualization.js
@@ -22,92 +22,114 @@ export function Visualization() {
     const id = params.id;
     const [state, setState] = useState({});
     return (
+
         <div
-            style={{
-                    backgroundColor: 'darkgray',
-                    }}>
-            <h1 style={{
-                    left: '120px',
-                    top: '55px',
-                    position: 'absolute',
-                    fontSize: '20px'
-                }}>Aggregate COVID-19 Immunization in Tanzania for the year {id},to {params.pe} {params.ou}!</h1>
+        style={{
+            height: "100%",
+            width: "100%",
+            backgroundColor: "#F5F5F5",
+            //color: "#fffff",
+            //fontSize: "24px",
+            textAlign: "center",
+            display: "flex",
+            alignItems: "center",
+            justifyContent: "center"
+            }}>
 
-            <div id='gobackbutton'
+                <div
                 style={{
-                    left: '30px',
-                    top: '60px',
                     position: 'absolute',
+                    top: 35,
+                    left: 0,
+                    margin: '1em',
                 }}>
-
-                <Button onClick={()=> navigate(-1)} name="Basic button" value="default">
+                <Button onClick={()=> navigate(-1)} name="Small button" small value="default">
                     Go Back
                 </Button>
-            </div>
-
-
-
-
-            <div
-                style={{
-                    backgroundColor: 'whitesmoke',
-                    border: '1px solid 1px solid 1px solid #C5E3FC',
-                    boxShadow: '0px 0px 5px 5px rgba(0,0,0,0.2)',
-                    height: '84%',
-                    width: '95%',
-                    display: 'flex',
-                    left: '35px',
-                    top: '100px',
-                    position: 'absolute',
-                    justifyContent:'center',
-                    alignItems: 'center',
-                }}>
-
-                <div id='topbutton'
-                     style={{
-                         display: 'flex',
-                         left: '-20px',
-                         top: '12px',
-                         position: 'absolute',
-                         paddingLeft: '25px',
-                     }}>
-                    <Button
-                        style={{
-                            paddingLeft: '100px',position:'absolute',
-                        }}
-                        disabled name="Disabled button"
-                        value="default">
-                        Save
-                    </Button>
-                    <DropdownButton
-                        component={<FlyoutMenu><MenuItem label="PNG" /><MenuItem label="PDF" /></FlyoutMenu>}
-                        name="buttonName"
-                        value="buttonValue"
-                        disabled ="Disabled button"
-                    >
-                        Export
-                    </DropdownButton>
-                    <Button
-                        disabled name="Disabled button"
-                        value="default">
-                        Settings
-                    </Button>
-                    <Button
-                        disabled name="Disabled button"
-                        value="default">
-                        Save to Dashboard
-                    </Button>
                 </div>
 
 
-                <PivotTableUI
-                    data={data}
-                    onChange={s => setState(s)}
-                    renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
-                    {...state}
-                />
+
+                <div>
+                <h1 style={{
+                    left: 120,
+                    top: 43,
+                    position: 'absolute',
+                    fontSize: '18px',
+                    //alignItems:'center'
+                }}>This will display the title of the data file Visualized {id},to {params.pe} {params.ou}!</h1>
+
+                </div>
+
+                <div
+                style={{
+                    height: "88%",
+                    width: "96%",
+                    backgroundColor: "white",
+                    //color: "#fffff",
+                    //fontSize: "24px",
+                    textAlign: "center",
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                    border: '1px solid 1px solid 1px solid #C5E3FC',
+                    boxShadow: '0px 0px 5px 5px rgba(0,0,0,0.3)',
+                }}>
+
+                <div
+                style={{
+                    position: 'absolute', 
+                    top: '14%', 
+                    left: '3%', 
+                    gap:'7',
+                    display: 'flex'
+                    }}>
+                <Button name="Small button" small value="default" > Save </Button>
+
+                <DropdownButton
+                component={
+                <FlyoutMenu>
+                    <MenuItem label="PNG" />
+                    <MenuItem label="PDF" /> 
+                </FlyoutMenu>
+                           }
+                name="buttonName"
+                small value="buttonValue"> Export
+                 </DropdownButton>
+
+                <Button name="Small button" small value="default" > Settings </Button>
+
+                <Button name="Small button" small value="default" > Save to Dashboard </Button>
             </div>
-    </div>
+
+
+
+
+                <div id='pivot' style={{backgroundColor:'greenyellow'}}>
+                <PivotTableUI
+                data={data}
+                onChange={s => setState(s)}
+                renderers={Object.assign({}, TableRenderers, PlotlyRenderers)}
+                {...state}
+         />    
+
+                
+        
+
+        </div>
+
+
+                </div>
+
+        </div>
+
+
+
+
+
+
+
+        
 
 
 
